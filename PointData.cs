569341d3e084245a2f705a55e1c3dd416e59e145
Radosław Boczoń <radoslaw.boczon@onet.eu>
2022-02-21 19:26:49 +0100

    Add project files.

---
+++ PointData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPoolR_Boczoń
+{
+    public class PointData
+    {
+        double x;
+        double y;
+        Color color;
+        public PointData(double x, double y, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
+}
---
+++ PointGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ThreadPoolR_Boczoń
+{
+    public class PointGenerator
+    {
+        private Color color;
+        private List<PointData> collection;
+        private Thread thread;
+        private long sleepTime;
+        public PointGenerator(Color color, List<PointData> collection, float sleep)
+        {
+            this.color = color;
+            this.collection = collection;
+            sleepTime = (long) sleep*TimeSpan.TicksPerMillisecond;
+            thread = new Thread(GeneratePoint);
+            thread.IsBackground = true;
+        }
+
+        private void GeneratePoint()
+        {
+            long start,
+                current;
+
+            start = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            System.Random rng = new Random();
+            
+            while (thread.IsAlive)
+            {
+                current= DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                if (start + sleepTime <= current)
+                {
+
+                    PointData point = new PointData(
+                        rng.NextDouble(),
+                        rng.NextDouble(),
+                        color
+                        );
+
+                    collection.Add(point);
+
+                    start = current;
+                }
+            }
+        }
+
+        public void StartGeneration() 
+        {
+            try
+            {
+                thread.Start();
+            }
+            catch (ThreadStartException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        public void StopGeneration()
+        {
+            try
+            {
+                thread.Suspend();
+            }
+            catch(ThreadStateException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+    }
+}

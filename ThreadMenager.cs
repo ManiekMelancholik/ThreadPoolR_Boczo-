d6990a6231859e0c04ca07220882603e9dee4709
Radosław Boczoń <radoslaw.boczon@onet.eu>
2022-02-22 23:16:39 +0100

    Merge branch 'master' of https://github.com/ManiekMelancholik/ThreadPoolR_Boczo-

---
+++ ThreadMenager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolR_Boczoń
+{
+    public static class ThreadMenager
+    {
+        private static Thread ThreadPoolMainThread = new Thread();
+        private static List<PointData> pointsInView = new List<PointData>();
+        private static List<PointData> pointsToAdd = new List<PointData>();
+
+
+        private static EventHandler eventHandler = new EventHandler();
+
+
+    }
+}
